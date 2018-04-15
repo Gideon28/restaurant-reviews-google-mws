81b9bc280439e7240d9aa01be1c40f3521f7589c
Gideon Caspi <gideoncaspi@gmail.com>
2018-04-15 17:58:18 +0100

    Added empty ServiceWorker lifecycle methods and cache name

---
+++ sw.js
@@ -0,0 +1,7 @@
+var staticCacheName = "restaurant-app-v1";
+
+self.addEventListener("install", function(event) {});
+
+self.addEventListener("activate", function(event) {});
+
+self.addEventListener("fetch", function(event) {});
