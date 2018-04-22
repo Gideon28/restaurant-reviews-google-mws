0098f249d07342c933316be17e89b4bdbbc63fd9
Gideon Caspi <gideoncaspi@gmail.com>
2018-04-22 22:14:36 +0100

    added to service worker script and attempted to factor it out for reuse - to fix

--- sw.js
+++ sw.js
@@ -1,7 +1,70 @@
-var staticCacheName = "restaurant-app-v1";
+const staticCacheName = 'restaurant-app-v3';
+const imagesCache = 'restaurant-app-images';
 
-self.addEventListener("install", function(event) {});
+const allCaches = [staticCacheName, imagesCache];
 
-self.addEventListener("activate", function(event) {});
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.addAll([
+        '/',
+        'js/main.js',
+        'css/styles.css',
+        'https://fonts.gstatic.com/s/roboto/v18/KFOmCnqEu92Fr1Mu4mxKKTU1Kg.woff2',
+      ]);
+    })
+  );
+});
 
-self.addEventListener("fetch", function(event) {});
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('restaurant-') && !allCaches.includes(cacheName);
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+function serveImage(request) {
+  const storageUrl = request.url.replace(/\.jpg$/, '');
+
+  return caches.open(imagesCache).then(cache =>
+    cache.match(storageUrl).then(response => {
+      if (response) return response;
+
+      return fetch(request).then(networkResponse => {
+        cache.put(storageUrl, networkResponse.clone());
+        return networkResponse;
+      });
+    })
+  );
+}
+
+self.addEventListener('fetch', event => {
+  const requestUrl = new URL(event.request.url);
+
+  if (requestUrl.origin === location.origin) {
+    //   if (requestUrl.pathname === '/') {
+    //     // event.respondWith(caches.match('/skeleton'));
+    //     console.log('on home page');
+    //   }
+
+    if (requestUrl.pathname.startsWith('/img/')) {
+      event.respondWith(serveImage(event.request));
+      return;
+    }
+  }
+
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+});
+
+self.addEventListener('message', event => {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
