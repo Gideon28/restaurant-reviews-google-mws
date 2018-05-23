7dc0c7a27725e4b6b56d57cf028d536b6bee465d
Gideon Caspi <gideoncaspi@gmail.com>
2018-05-23 21:45:43 +0100

    set up linting and some initial automation

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+var eslint = require('gulp-eslint');
+
+gulp.task('default', ['styles', 'lint'], function() {
+  gulp.watch('sass/**/*/*.scss', ['styles']);
+  gulp.watch('js/**/*.js', ['lint']);
+
+  browserSync.init({
+    server: './'
+  });
+});
+
+gulp.task('styles', function() {
+  gulp.src('sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions']
+    }))
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('lint', function() {
+  return gulp.src(['js/**/*.js'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failOnError());
+});
